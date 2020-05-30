d38b81dc9474b0ac779121f161020363f686c7b3
LeoSL <leosilvalima@gmail.com>
2020-05-30 16:11:37 -0400

    Add fetch URL exercises

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -9,25 +9,62 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
+	// withReadAll()
+	withCopy()
+}
+
+// func withReadAll() {
+// 	start := time.Now()
+// 	for _, url := range os.Args[1:] {
+// 		resp, err := http.Get(url)
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+// 			os.Exit(1)
+// 		}
+// 		b, err := ioutil.ReadAll(resp.Body)
+
+// 		resp.Body.Close()
+// 		if err != nil {
+// 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+// 			os.Exit(1)
+// 		}
+// 		fmt.Printf("%s", b)
+// 		fmt.Printf("%.10fs elapsed\n", time.Since(start).Seconds())
+// 	}
+// }
+
+func withCopy() {
+	start := time.Now()
 	for _, url := range os.Args[1:] {
+		if !(strings.HasPrefix(url, "http://")) {
+			url = "http://" + url
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+
+		// writing to os.Stdout is better because it doesn't require
+		// an initial buffer large enough to hold the entire stream
+		_, err = io.Copy(os.Stdout, resp.Body)
+
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		fmt.Printf("%.10fs elapsed\n", time.Since(start).Seconds())
+		fmt.Println("REQUEST STATUS: " + resp.Status)
 	}
 }
 
